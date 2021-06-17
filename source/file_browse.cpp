255590be11a36f870534137f50546c9d4f8c6ab0
RocketRobz <bobesh8@gmail.com>
2021-06-17 14:17:29 -0600

    Bug fix

--- source/file_browse.cpp
+++ source/file_browse.cpp
@@ -92,7 +92,7 @@ void getDirectoryContents (vector<DirEntry>& dirContents, const vector<string> e
 			dirEntry.name = pent->d_name;
 			dirEntry.isDirectory = (st.st_mode & S_IFDIR) ? true : false;
 
-			if (dirEntry.name.compare(".") != 0 && !dirEntry.isDirectory && nameEndsWith(dirEntry.name, extensionList)) {
+			if (dirEntry.name.compare(".") != 0 && nameEndsWith(dirEntry.name, extensionList)) {
 				for (int i = dirEntry.name.size(); i > 0; i--) {
 					if (dirEntry.name[i] == '.') {
 						dirEntry.name[i] = '\0';	// Hide file extension from display
